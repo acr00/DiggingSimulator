1960629bc6d84c655998c71f58db1707f97dab0a
acr00 <adriancainas@gmail.com>
2024-12-06 17:13:51 +0100

    added proyect logic files to the repository

---
+++ src/main/java/dev/acr/diggingsimulator/DiggingsimulatorApplication.java
@@ -0,0 +1,13 @@
+package dev.acr.diggingsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiggingsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiggingsimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/acr/diggingsimulator/DiggingsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.acr.diggingsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiggingsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
